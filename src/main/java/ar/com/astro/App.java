d604b60715deb03e58e78e8ba9eaaad348774c0b
Astro <astroatmoky@gmail.com>
2018-12-22 13:50:31 -0300

    First commit

---
+++ src/main/java/ar/com/astro/App.java
@@ -0,0 +1,21 @@
+package ar.com.astro;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        final String name =  StringUtils.capitalize("juan");
+        System.out.println( "Hello World!"  );
+
+        System.out.println(name);
+
+
+    }
+}
